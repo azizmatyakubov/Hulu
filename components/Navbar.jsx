89a942da937c4d73974808911ff7ef86a2852d4c
AZIZ <a.matyaqubov1471@gmail.com>
2022-11-10 12:16:53 +0100

    add onClick to nav items

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,14 @@
 import request from "../utils/request";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
+  const router = useRouter();
   return (
     <nav className="relative">
       <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide ">
         {Object.entries(request).map(([key, { title, url }]) => (
           <h2
+            onClick={() => router.push(`/?genre=${key}`)}
             key={key}
             className="cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500"
           >
